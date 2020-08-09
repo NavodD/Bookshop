df9f279e2452d4216d9329991bc252ce274ee7ba
medila <69413190+medila@users.noreply.github.com>
2020-08-09 11:49:57 +0530

    Add files via upload

---
+++ lib/pages/Cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../packages/CartProducts.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Cart'),
+      actions: <Widget>[
+       
+        new IconButton(icon: Icon(Icons.search,color: Colors.white) , onPressed: null, ),
+       
+      ],
+      ),
+
+    body: new CartProducts(),
+
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                title: new Text("Total:"),
+                subtitle: new Text("\$235"),
+              ),
+            ),
+            Expanded(
+              child: new MaterialButton(onPressed: (){},
+              child: new Text("Cheack Out", style: TextStyle(color: Colors.white)),
+              color: Colors.red,
+              ),
+              
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
