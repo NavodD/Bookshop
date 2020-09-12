0e67d11125eb5552eab8129e7df71a8d27e40d89
chamakaathukorala <s16316@sci.pdn.ac.lk>
2020-09-13 01:33:22 +0530

    Added new images

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shopapp/pages/Login.dart';
+import './pages/Login.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BOOK SHOP',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold( );
-  }
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    )
+  );
 }
